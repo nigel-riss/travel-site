999ca82704a810209d6bcd847d065dab276ca25f
nigel-riss <mr.kurenkov@gmail.com>
2017-02-08 12:48:01 +0200

    Completed desktop and mobile header styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
-        path: "./app/temp/scripts",
+        // path: "./app/temp/scripts",
         filename: "App.js"
     }
     ,
